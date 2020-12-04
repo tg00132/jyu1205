51790a57b66d48813f0a5dcf81bbab75c74ad38c
tg00132 <75109813+tg00132@users.noreply.github.com>
2020-12-04 20:58:07 +0900

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,12 @@
+package hkhkhkhkhk;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("임수환 바보");
+		System.out.println("바보");
+
+	}
+
+}
